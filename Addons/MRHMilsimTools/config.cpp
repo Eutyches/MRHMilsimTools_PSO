613059c7b6e4e69494517fcb2719a73b866c49aa
big-corey <39989971+big-corey@users.noreply.github.com>
2019-08-18 05:05:13 -0600

    PSO Skins 0.5.1

--- Addons/MRHMilsimTools/config.cpp
+++ Addons/MRHMilsimTools/config.cpp
@@ -92,6 +92,126 @@ class CfgUnitInsignia
 		displayName = $STR_MRH_MISC_MedicPatchInsignia;
 		texture = "\MRHMilSimTools\paa\medicpatch.paa";
 	};
+	class PSO_UnitPatch_Flag
+	{
+		author = "Corey";
+		displayName = "PSO Flag";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_flag.paa";
+	};
+	class PSO_UnitPatch_Helmet
+	{
+		author = "Corey";
+		displayName = "PSO Helmet (Color)";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_helmet.paa";
+	};
+	class PSO_UnitPatch_Helmet_Black
+	{
+		author = "Corey";
+		displayName = "PSO Helmet (Black)";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_helmet_black.paa";
+	};
+	class PSO_UnitPatch_Helmet_BlackonWhite
+	{
+		author = "Corey";
+		displayName = "PSO Helmet (Black on White)";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_helmet_blackonwhite.paa";
+	};
+	class PSO_UnitPatch_Helmet_White
+	{
+		author = "Corey";
+		displayName = "PSO Helmet (White)";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_helmet_white.paa";
+	};
+	class PSO_UnitPatch_Helmet_WhiteonBlack
+	{
+		author = "Corey";
+		displayName = "PSO Helmet (White on Black)";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_helmet_whiteonblack.paa";
+	};
+	class PSO_UnitPatch_MinstrelDuck
+	{
+		author = "Corey";
+		displayName = "PSO Minstrel Duck";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_minstrelduck.paa";
+	};
+	class PSO_UnitPatch_Patch
+	{
+		author = "Corey";
+		displayName = "PSO Unit Patch";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_patch.paa";
+	};
+	class PSO_UnitPatch_Snack
+	{
+		author = "Corey";
+		displayName = "PSO Snackbar";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_snack.paa";
+	};
+	class PSO_UnitPatch_Text1
+	{
+		author = "Corey";
+		displayName = "PSO Full Text Black";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_text1.paa";
+	};
+	class PSO_UnitPatch_Text1_1
+	{
+		author = "Corey";
+		displayName = "PSO Full Text White";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_text1_1.paa";
+	};
+	class PSO_UnitPatch_Text1_2
+	{
+		author = "Corey";
+		displayName = "PSO Full Text Black on White";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_text1_2.paa";
+	};
+	class PSO_UnitPatch_Text1_3
+	{
+		author = "Corey";
+		displayName = "PSO Full Text White on Black";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_text1_3.paa";
+	};
+	class PSO_UnitPatch_Text2
+	{
+		author = "Corey";
+		displayName = "PSO Text Black";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_text2.paa";
+	};
+	class PSO_UnitPatch_Text2_1
+	{
+		author = "Corey";
+		displayName = "PSO Text White";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_text2_1.paa";
+	};
+	class PSO_UnitPatch_Text3
+	{
+		author = "Corey";
+		displayName = "PSO Text Black on White";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_text3.paa";
+	};
+	class PSO_UnitPatch_Text4
+	{
+		author = "Corey";
+		displayName = "PSO Text White on Black";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_text4.paa";
+	};
+	class PSO_UnitPatch_War
+	{
+		author = "Corey";
+		displayName = "PSO War is Hell";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_war.paa";
+	};
+	class PSO_UnitPatch_War2
+	{
+		author = "Corey";
+		displayName = "PSO War is Hell Black on White";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_war2.paa";
+	};
+	class PSO_UnitPatch_War3
+	{
+		author = "Corey";
+		displayName = "PSO War is Hell White on Black";
+		texture = "\MRHMilSimTools\paa\unitpatch_pso_war3.paa";
+	};
 };
 class Extended_Init_EventHandlers {
     class CAManBase {
